7bea25d892b5efe3d79bef29e7bb6fcb08938ba7
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-04-05 08:11:46 +0530

    Deployment attempt 2

--- HabitTracker_Backend/src/main/java/com/cnl/habittracker_backend/exception/GlobalExceptionHandler.java
+++ HabitTracker_Backend/src/main/java/com/cnl/habittracker_backend/exception/GlobalExceptionHandler.java
@@ -6,12 +6,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ApiError> handleBadRequest(BadRequestException ex) {
@@ -55,6 +59,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleGeneric(Exception ex) {
+        logger.error("Unhandled exception", ex);
         return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error");
     }
 
